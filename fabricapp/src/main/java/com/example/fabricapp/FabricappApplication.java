f33f68f46b5b85d3fb11cde1b75b3ff346e738ca
jasoninsun <1025262651@qq.com>
2018-08-14 11:00:08 +0800

    init

---
+++ fabricapp/src/main/java/com/example/fabricapp/FabricappApplication.java
@@ -0,0 +1,12 @@
+package com.example.fabricapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricappApplication.class, args);
+	}
+}
