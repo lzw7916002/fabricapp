d09180a83e323addb5bfdc3c1d6b0f6934914a7f
jasoninsun <1025262651@qq.com>
2018-08-14 11:31:30 +0800

    init

---
+++ farbicapp/src/main/java/farbicapp/FarbicappApplication.java
@@ -0,0 +1,12 @@
+package farbicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarbicappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarbicappApplication.class, args);
+	}
+}
---
+++ farbicapp/src/test/java/farbicapp/FarbicappApplicationTests.java
@@ -0,0 +1,16 @@
+package farbicapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FarbicappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
